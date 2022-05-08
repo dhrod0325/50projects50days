54fe575d333aac49f1a3287dad65e4dbc503caac
설아아빠 <dhrod0325@naver.com>
2022-05-08 16:52:19 +0900

    refactor(hidden-search) : 토글로 변경

--- hidden-search/main.js
+++ hidden-search/main.js
@@ -1,9 +1,5 @@
 const $container = document.querySelector('.hidden-search');
 
 document.querySelector('button').addEventListener('click', () => {
-    if ($container.classList.contains('hidden')) {
-        $container.classList.remove('hidden');
-    } else {
-        $container.classList.add('hidden');
-    }
+    $container.classList.toggle('hidden');
 });
